e469855f3026efdf53d6ac50012bb85df11aea85
Richard Williams <richard.lluttrell@gmail.com>
2020-02-03 19:11:35 -0700

    caculator works but doesn't have a zero button . Also needs decimals rounded.

--- calculator.js
+++ calculator.js
@@ -1,9 +1,11 @@
 let displayValue = "5138008";
 let firstValue = 0;
 let secondValue = 0;
+let currentOperation = null;
 
 updateScreen(displayValue);
 
+// add values to screen as user inputs numbers
 document.getElementById("clear").addEventListener('click', clearAll);
 document.getElementById("one").addEventListener('click', updateScreenValue.bind(null,"1"));
 document.getElementById("two").addEventListener('click', updateScreenValue.bind(null,"2"));
@@ -15,9 +17,49 @@ document.getElementById("seven").addEventListener('click', updateScreenValue.bin
 document.getElementById("eight").addEventListener('click', updateScreenValue.bind(null,"8"));
 document.getElementById("nine").addEventListener('click', updateScreenValue.bind(null,"9"));
 
-function updateScreenValue(input) {
-    displayValue += input;
+// record first number and set operation flag
+document.getElementById("plus").addEventListener('click', operationSelect.bind(null,"plus"));
+document.getElementById("minus").addEventListener('click', operationSelect.bind(null,"minus"));
+document.getElementById("multiply").addEventListener('click', operationSelect.bind(null,"multiply"));
+document.getElementById("divide").addEventListener('click', operationSelect.bind(null,"divide"));
+
+// display result if equal selected
+document.getElementById("equals").addEventListener('click', performCalculation);
+
+function performCalculation() {
+    secondValue = parseInt(displayValue);
+    switch (currentOperation) {
+        case "plus":
+            displayValue = firstValue + secondValue;
+            break;
+        case "minus":
+            displayValue = firstValue - secondValue;
+            break;
+        case "multiply":
+            displayValue = firstValue * secondValue;
+            break;
+        case "divide":
+            displayValue = firstValue / secondValue;
+            break;
+        default:
+            displayValue = firstValue;        
+    }
     updateScreen(displayValue);
+    console.log("foo");
+}
+
+function operationSelect(operation) {
+    currentOperation = operation;
+    firstValue = parseInt(displayValue);
+    displayValue = "";
+    updateScreen(displayValue);
+}
+
+function updateScreenValue(input) {
+    if (displayValue.length <= 8) {
+        displayValue += input;
+        updateScreen(displayValue);
+    }
 }
 
 function updateScreen(displayValue) {
@@ -29,5 +71,4 @@ function clearAll() {
     secondValue = 0;
     displayValue = "";
     updateScreen(displayValue);
-    console.log("cleared");
 }
